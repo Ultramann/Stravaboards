5797ebe0617afd80b18a7e8ad8290854178a2523
Ultramann <cary.goltermann@gmail.com>
2015-08-27 10:17:14 -0600

    Playing with strava api

--- strava/test.py
+++ strava/test.py
@@ -4,6 +4,6 @@ access_token = '221492179ffd21cc31da6928b13b6ab647c83fc1'
 extra_headers = {'Authorization' : 'Bearer %s' % access_token}
 
 url_base = 'https://www.strava.com/api/v3/'
-r1 = requests.get(url_base + 'activities/321934', headers=extra_headers)
-r2 = requests.get(url_base + 'athletes/{}/stats'.format(r1.json()['athlete']['id']), 
+#r1 = requests.get(url_base + 'activities/91314163', headers=extra_headers)
+r2 = requests.get(url_base + 'segments/{}/all_efforts'.format(1030300),
                  headers=extra_headers)
