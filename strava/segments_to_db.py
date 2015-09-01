0cfe55e23e9fb20e30c56e0f030f1e2694427965
Ultramann <cary.goltermann@gmail.com>
2015-09-01 17:21:23 -0600

    Pretty printing

--- strava/segments_to_db.py
+++ strava/segments_to_db.py
@@ -9,8 +9,11 @@ if __name__ == '__main__':
                 626742, 8643847, 1154584, 3664904, 7330795, 809335, 857898, 954038, 600237, 1262370,
                 1549153, 954038, 618199, 1741438, 719341, 638232, 949453, 997901, 3883126, 7197693]
 
-    for segment in segments:
+    for i, segment in enumerate(segments):
+        efforts = get_segment_efforts(segments)
+        print 'Got efforts for segment {}'.format(i)
         for effort in get_segment_efforts(segment):
             if (isinstance(effort, dict) and isinstance(effort['activity'], dict)):
                 if not table.find_one({'activity.id': effort['activity']['id']}):
                     table.insert_one(effort)
+        print '\tDone uploading efforts for segment {}'.format(i)
