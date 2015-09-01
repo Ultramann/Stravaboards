1563b64e09f85318bb7786f0b93e14908e1a7973
Ultramann <cary.goltermann@gmail.com>
2015-09-01 17:10:43 -0600

    Working on getting lots of segments efforts into mongo

--- strava/segments_to_db.py
+++ strava/segments_to_db.py
@@ -5,9 +5,12 @@ if __name__ == '__main__':
     client = MongoClient()
     db = client['Strava']
     table = db['Segment_Efforts']
-    segments = [6366843, 617239, 904763, 1173191, 1723, 611363, 563888, 2386958, 3490822, 5264177, 626742, 8643847, 1154584, 3664904, 
+    segments = [6366843, 617239, 904763, 1173191, 1723, 611363, 563888, 2386958, 3490822, 5264177, 
+                626742, 8643847, 1154584, 3664904, 7330795, 809335, 857898, 954038, 600237, 1262370,
+                1549153, 954038, 618199, 1741438, 719341, 638232, 949453, 997901, 3883126, 7197693]
 
     for segment in segments:
         for effort in get_segment_efforts(segment):
-            if not table.find_one({'activity.id': effort['activity']['id']}):
-                table.insert_one(effort)
+            if (isinstance(effort, dict) and isinstance(effort['activity'], dict)):
+                if not table.find_one({'activity.id': effort['activity']['id']}):
+                    table.insert_one(effort)
