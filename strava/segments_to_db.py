7ef485eb5c8592c607618fd01b7f719cadc29ed9
Ultramann <cary.goltermann@gmail.com>
2015-09-01 19:26:48 -0600

    Trying to do parallel processing

--- strava/segments_to_db.py
+++ strava/segments_to_db.py
@@ -1,19 +1,23 @@
 from pymongo import MongoClient
+from multiprocessing import Pool
 from strava_lib import get_segment_efforts
+
+def get_insert_segment_efforts(segment):
+    efforts = get_segment_efforts(segment)
+    for effort in efforts:
+        if (isinstance(effort, dict) and isinstance(effort['activity'], dict)):
+            if not table.find_one({'activity.id': effort['activity']['id']}):
+                table.insert_one(effort)
+
 
 if __name__ == '__main__':
     client = MongoClient()
     db = client['Strava']
     table = db['Segment_Efforts']
-    segments = [6366843, 617239, 904763, 1173191, 1723, 611363, 563888, 2386958, 3490822, 5264177, 
+    segments = [1173191, 1723, 611363, 563888, 2386958, 3490822, 5264177, 
                 626742, 8643847, 1154584, 3664904, 7330795, 809335, 857898, 954038, 600237, 1262370,
                 1549153, 954038, 618199, 1741438, 719341, 638232, 949453, 997901, 3883126, 7197693]
+    finished_segments = [6366843, 617239, 904763]
 
-    for i, segment in enumerate(segments):
-        efforts = get_segment_efforts(segments)
-        print 'Got efforts for segment {}'.format(i)
-        for effort in get_segment_efforts(segment):
-            if (isinstance(effort, dict) and isinstance(effort['activity'], dict)):
-                if not table.find_one({'activity.id': effort['activity']['id']}):
-                    table.insert_one(effort)
-        print '\tDone uploading efforts for segment {}'.format(i)
+    pool = Pool(processes=2)
+    pool.map(get_insert_segment_efforts, segments)
