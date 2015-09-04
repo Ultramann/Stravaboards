642d1768d202f50014a698c69bb5a3e25f79b35c
Ultramann <cary.goltermann@gmail.com>
2015-09-04 16:38:16 -0600

    Fixing some data collection blunders

--- segments_to_db.py
+++ segments_to_db.py
@@ -2,7 +2,7 @@ import time
 import json
 import requests
 from pymongo import MongoClient
-from multiprocessing import Pool
+from pymongo.bulk import BulkOperationBuilder
 
 with open('./.strava.json') as f:
     data = json.loads(f.read())
@@ -14,9 +14,9 @@ page_max = 200
 
 client = MongoClient()
 db = client['Strava']
-table = db['Segment_Efforts']
+table = db['segment_efforts']
 
-def get_page_efforts(page):
+def get_page_efforts(page, segment):
     return requests.get(url_base + 'segments/{}/all_efforts'.format(segment), 
                         headers=header, 
                         params={'per_page': page_max, 'page': page}).json()
@@ -24,47 +24,39 @@ def get_page_efforts(page):
 def get_segment_efforts(segment):
     seg_eff_cnt = requests.get(url_base + 'segments/{}'.format(segment), 
                                headers=header).json()['effort_count']
+    return [effort for page in range(1, seg_eff_cnt/page_max + 2)
+                   for effort in get_page_efforts(page, segment)]
 
-    pool = Pool(processes=4)
-    all_efforts = pool.map(get_page_efforts, range(1, seg_eff_cnt/page_max + 2))
-    return [effort for effort_list in all_efforts for effort in effort_list]
-
-def insert_effort(effort):
-    #if (isinstance(effort, dict) and isinstance(effort['activity'], dict)):
-    #    if not table.find_one({'activity.id': effort['activity']['id']}):
-    if not table.find_one({'_id': effort['_id']}):
-        table.insert_one(effort)
+def insert_efforts(efforts):
+    table.insert_many(efforts)
 
 def get_insert_segment_efforts(segment):
     t1 = time.time()
     efforts = get_segment_efforts(segment)
     print '   Retrieved {} efforts in {:.2f} minutes'.format(len(efforts), (time.time() - t1)/60)
     t2 = time.time()
-    pool = Pool(processes=4)
-    pool.map(insert_effort, efforts)
+    insert_efforts(efforts)
     print '   Inserted them into database in {:.2f} minutes'.format((time.time() - t2)/60)
     return len(efforts)
 
 if __name__ == '__main__':
-    whistler_segments = {7798146, 5277046, 4737543, 1455733, 2672001, 1466521, 633911, 718366, 
-                         4727168, 9500933, 2247528, 9022063, 5595558, 1335898, 633905, 1667753,
-                         7201648, 4761904, 5281321, 7874172, 689120, 4967549, 5266694, 1704074, 
-                         1810311, 8061367, 1441248, 1441248}
-
-    # Set of segements data has been collected on from: North of SF, Moab
-    finished_segments = {825868, 7966813, 7318092, 759935, 3919800, 3783490, 1310387, 721666, 763617,
-                         2575735, 2575792, 4009312, 2211929, 655907, 1167465, 1552300, 712936, 
-                         9395178, 721666, 9197280, 3783466, 8483439, 3783439, 4000958, 7059167, 
-                         9918993, 6366843, 617239, 904763, 1173191, 1723, 611363, 563888, 3490822, 
-                         5264177, 626742, 8643847, 1154584, 3664904, 7330795, 809335, 857898, 954038,
-                         600237, 1262370, 1549153, 954038, 618199, 1741438, 719341, 638232, 949453,
-                         997901, 3883126, 7197693}
+    segments = {125, 5642079, 5857327, 4793848, 6048743, 118, 3305098, 356635, 4173351, 4062646, 
+                640981, 6135256, 1173191, 4783121, 1723, 6366843, 8429549, 7481858, 651728, 1521, 
+                4259807, 6875972, 5611730, 2707292, 7883627, 622149, 2451142, 5836703, 2858097}
+                #634332, 1077, 8411114, 881888, 3545515, 5099924, 3066267, 3066267, 934409, 633435,
+                #1003240, 866902, 835833, 8042617, 8727433, 4599878, 6325954, 1759580, 1105154, 
+                #806005, 1451654, 7531032, 4779241, 2435434, 841251, 2958707, 9008146, 2188435, 
+                #2627, 6838822, 5079282, 8594904, 7615757, 7615757, 2350753, 2687319, 2350791, 
+                #2339624, 2687221, 8727940, 844654, 1213534, 5831003, 5134503, 8050750, 798887, 
+                #7074191, 975395, 612159, 7969432, 1745022, 5292307, 180, 6768781, 5292307, 
+                #4178476, 925819, 925819, 6173812, 6458467, 4367683, 821934, 866323, 8800675, 
+                #666315, 2665113, 7186902, 6546684, 2234914, 747045, 8239228, 905184, 3200333, 
+                #1329495, 814196, 991174, 852755, 8692386, 8285294, 3559004, 6478150, 1042514,
+                #774591, 351211}
     
-    new_segments = [segment for segment in whistler_segments if segment not in finished_segments]
-
     total_efforts = 0
     t_init = time.time()
-    for i, segment in enumerate(new_segments):
+    for i, segment in enumerate(segments):
         print 'Starting segment {}...'.format(i)
         total_efforts += get_insert_segment_efforts(segment)
     t_mins = (time.time() - t_init)/60
