88c6b1ebfc32d2c3c92d8263161b0fa123f36e0d
Ultramann <cary.goltermann@gmail.com>
2015-09-07 13:30:01 -0600

    Made separate eda script for running on ec2 instance

---
+++ eda/cloud_eda.py
@@ -0,0 +1,10 @@
+import time
+import eda_fuctions
+from strava_db import EffortDfGetter
+
+if __name__ == '__main__':
+    t = time.time()
+    df_getter = EffortDfGetter(origin='json')
+    df = df_getter.get()
+    print 'Retrieved dataframe with {} efforts in {:.2f} seconds'.format(df.shape[0], time.time()-t)
+
