8546c3d2190e40f013ec717d488e1f9b63f11abe
Ultramann <cary.goltermann@gmail.com>
2015-09-07 13:26:34 -0600

    Moved eda functions to separate file

---
+++ eda/eda_functions.py
@@ -0,0 +1,28 @@
+import seaborn
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def print_num_atheletes_with_efforts_gt(df, nums):
+    for num in nums:
+        num_aths = len(df.groupby('athlete_id').count().query('activity >= {}'.format(num)))
+        print 'Number of athletes with greater than {} efforts in the database: {}'.format(
+                                                                                num, num_aths)
+
+def plot_num_athletes_by_effort_count_hist(df, bin_list=[1, 2, 3, 4, 11, 26, 51, 101, 100000], 
+                       bin_labels=['1', '2', '3', '4-10', '11-25', '26-50', '51-100', '101+'],
+                       show=True, save=False, file_name=None):
+    counts_by_athlete = pd.Series(df.groupby('athlete_id').count().date, name='effort_count')
+    count_series = counts_by_athlete.groupby(pd.cut(counts_by_athlete, bin_list, 
+                                                    labels=bin_labels)).count()
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    count_series.plot(kind='bar', axes=ax, grid=False)
+    plt.xlabel('Number of efforts in database')
+    plt.ylabel('Number of athletes')
+    plt.title('Athletes by effort count')
+    ax.set_xticklabels(bin_labels, rotation=0)
+    if not show:
+        plt.close()
+    if save and file_name:
+        fig.savefig(file_name)
+
