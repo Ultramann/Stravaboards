7b4c70b24a5d738945177b9568a0397ff6564f9c
Ultramann <cary.goltermann@gmail.com>
2015-09-09 14:45:15 -0600

    Started to add handling of multiple latent features

--- modeling/transform.py
+++ modeling/transform.py
@@ -14,28 +14,30 @@ def get_simple_df(df):
                           columns=['athlete_id'], aggfunc=np.mean)
     return pivot_df.unstack().reset_index(name='average_speed')
 
-def make_cleaner_dfs(dfs):
-    return [pd.concat([df, pd.Series(df.factors.apply(lambda x: x[0]), name='rating')], axis=1)
-            for df in dfs]
+def make_cleaner_dfs(dfs, num_features):
+    return [pd.concat([df, pd.Series(df.factors.apply(lambda x: x[i]), 
+                                     name='rating {}'.format(i+1))],
+                      axis=1) for i in range(num_features) for df in dfs]
 
 def drop_useless_columns(dfs):
     for df in dfs:
         df.drop(['linear_terms', 'factors'], axis=1, inplace=True)
 
-def get_clean_dfs_from_model(model):
+def get_clean_dfs_from_model(model, num_features):
     model_coefficients = model['coefficients']
     athlete_df = model_coefficients['athlete_id'].to_dataframe()
     segment_df = model_coefficients['segment_id'].to_dataframe()
-    cleaner_athlete_df, cleaner_segment_df = make_cleaner_dfs([athlete_df, segment_df])
+    cleaner_athlete_df, cleaner_segment_df = make_cleaner_dfs([athlete_df, segment_df], num_features)
     drop_useless_columns([cleaner_athlete_df, cleaner_segment_df])
     cleaner_athlete_df.set_index(['athlete_id'], inplace=True)
     cleaner_segment_df.set_index(['segment_id'], inplace=True)
     return cleaner_athlete_df, cleaner_segment_df
 
 def get_latent_features(sf):
+    number_latent_features = 2
     model = gl.factorization_recommender.create(sf, user_id='athlete_id', item_id='segment_id', 
-                                                target='average_speed', num_factors=1)
-    athlete_ratings, segment_ratings = get_clean_dfs_from_model(model)
+                                        target='average_speed', num_factors=number_latent_features)
+    athlete_ratings, segment_ratings = get_clean_dfs_from_model(model, number_latent_features)
     return athlete_ratings, segment_ratings, model
 
 if __name__ == '__main__':
