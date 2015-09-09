97aaa00d2e03cfe408035052a8146b36afd8c2e3
Ubuntu <ubuntu@ip-172-31-24-75.us-west-1.compute.internal>
2015-09-09 18:48:35 +0000

    Making df that works with GraphLab's recommender

--- modeling/transform.py
+++ modeling/transform.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('../eda')
 from strava_db import EffortDfGetter
+import graphlab as gl
 import pandas as pd
 import numpy as np
 
@@ -11,7 +12,11 @@ def get_df():
 def get_pivot_df(df):
     return pd.pivot_table(df, values='average_speed', index='athlete_id', 
                           columns=['segment_id'], aggfunc=np.mean)
+def get_simple_df(df):
+    return df.unstack().reset_index(name='average_speed')
 
 if __name__ == '__main__':
     df = get_df()
     pivot_df = get_pivot_df(df)
+    simple_df = get_simple_df(pivot_df)
+    sf = gl.SFrame(simple_df)
