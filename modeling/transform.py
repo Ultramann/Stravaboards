1dbd73f15e1d25dbbd4e66e9a576249e977871fa
Ubuntu <ubuntu@ip-172-31-24-75.us-west-1.compute.internal>
2015-09-09 17:56:48 +0000

    Made pivot table!!!

--- modeling/transform.py
+++ modeling/transform.py
@@ -2,10 +2,16 @@ import sys
 sys.path.append('../eda')
 from strava_db import EffortDfGetter
 import pandas as pd
+import numpy as np
 
 def get_df():
     df_getter = EffortDfGetter(origin='json')
     return df_getter.get()
 
+def get_pivot_df(df):
+    return pd.pivot_table(df, values='average_speed', index='athlete_id', 
+                          columns=['segment_id'], aggfunc=np.mean)
+
 if __name__ == '__main__':
     df = get_df()
+    pivot_df = get_pivot_df(df)
