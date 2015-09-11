cf5309cc49e557b1c784873fdec1579d52cd8da9
Ultramann <cary.goltermann@gmail.com>
2015-09-11 23:43:12 +0000

    Updated split_efforts to have more intellegent split

--- modeling/model_validation.py
+++ modeling/model_validation.py
@@ -1,20 +1,25 @@
 import random
+import pandas as pd
 
-def split_efforts(df, count=2000, gt_value=50, total_frac=0.5):
+def split_efforts(df, date='2015-08-01'):
     '''
     Input:  Full effort DataFrame
     Output: Training effort DF, Testing effort DF
 
-    Chooses subset of efforts filtered by users who have more than one effort on a segment.
+    Chooses subset of efforts filtered by date, so long as the filtered efforts for a single user 
+    do not represent all of that users segments or all of a segments efforts.
     '''
-    high_effort_count_df = df.groupby(['athlete_id', 'segment_id']).count()
-    athlete_segment_pairs = random.sample(high_effort_count_df.query('date > {}'.format(
-                                                                    gt_value)).index, count)
+    recent_efforts_df = df.query('date > "{}"'.format(date))
+    re_counts = pd.Series(recent_efforts_df.groupby(['athlete_id', 'segment_id']).count().date)
+    tot_counts = pd.Series(df.groupby(['athlete_id', 'segment_id']).count().date.ix[re_counts.index])
+
+    athlete_segment_pairs = re_counts[~(re_counts == tot_counts)].index
     drop_mask = [index for athlete, segment in athlete_segment_pairs
-                       for index in random.sample(df.query('athlete_id == {} & segment_id == {}' \
-                                                           .format(athlete, segment)).index.values,
-                                                  int(gt_value * total_frac))]
+                       for index in recent_efforts_df.query('athlete_id == {} & segment_id == {}'\
+                                             .format(athlete, segment)).index.values]
+
     testing_df = df.ix[drop_mask]
     training_df = df.drop(drop_mask)
+
     return training_df, testing_df
 
