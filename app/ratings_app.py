355f70d7f78f92080331e659f6c0e44e9bbf326e
Ultramann <cary.goltermann@gmail.com>
2015-09-12 21:59:14 +0000

    Getting ready to make leaderboard template

--- app/ratings_app.py
+++ app/ratings_app.py
@@ -6,9 +6,11 @@ app = Flask(__name__)
 @app.route('/leaderboards')
 def display_leaderboards():
     # Unpickle leaderboard list
-    lb_file_path = 'leaderboards.pkl'
+    lb_file_path = 'app_data/leaderboards.pkl'
     with open(lb_file_path, 'r') as f:
         leaderboards = pickle.load(f)
+
+    return render_template('leaderboards.html', leaderboard=leaderboards[0])
 
 if __name__ == '__main__':
     app.run(debug=True)
