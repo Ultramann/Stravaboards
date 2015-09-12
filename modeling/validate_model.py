b69dd51f299a7008ff8deaf28af88e852feb0d00
Ultramann <cary.goltermann@gmail.com>
2015-09-12 00:01:46 -0600

    Added comments to everything. Tried fixing split_efforts

--- modeling/validate_model.py
+++ modeling/validate_model.py
@@ -2,7 +2,6 @@ import random
 import numpy as np
 import pandas as pd
 import graphlab as gl
-import create_model as cm
 
 def plot_ratings(ratings_df):
     '''
@@ -26,11 +25,21 @@ def evaluate_latent_feature_correlations(df, segment_ratings):
     Input: Training DataFrame, DataFrame of segment ratings
     Output: Correlation matrix for the ratings from segment_ratings
     '''
+    # Get list of rating column names
     rating_columns = list(segment_ratings.columns.values) 
+
+    # Make list of all columns to grab from full dataframe
     segment_columns = ['seg_average_grade', 'seg_distance', 'seg_maximum_grade'] + rating_columns
                        
-    return df.groupby('segment_id').first().merge(segment_ratings, right_index=True, 
-                                   left_index=True)[segment_columns].corr()[rating_columns]
+    # Get one instance of all info for each segment
+    segment_df = df.groupby('segment_id').first()
+
+    # Join segment info with the latent feature ratings for each segment
+    segment_correlation = segment_df.merge(segment_ratings, right_index=True, 
+                                           left_index=True)[segment_columns].corr()
+
+    # Return just the rating columns from the correlation df
+    return segment_correlation[rating_columns]
 
 def split_efforts(df, date='2015-08-01'):
     '''
@@ -40,17 +49,22 @@ def split_efforts(df, date='2015-08-01'):
     Chooses subset of efforts filtered by date, so long as the filtered efforts for a single user 
     do not represent all of that users segments or all of a segments efforts.
     '''
+    # Get dataframe of all efforts that occurred after "date"
     recent_efforts_df = df.query('date > "{}"'.format(date))
-    re_counts = pd.Series(recent_efforts_df.groupby(['athlete_id', 'segment_id']).count().date)
-    tot_counts = pd.Series(df.groupby(['athlete_id', 'segment_id']).count().date.ix[re_counts.index])
 
-    athlete_segment_pairs = re_counts[~(re_counts == tot_counts)].index
-    drop_mask = [index for athlete, segment in athlete_segment_pairs
-                       for index in recent_efforts_df.query('athlete_id == {} & segment_id == {}'\
-                                             .format(athlete, segment)).index.values]
+    # Get the segment counts for each athlete after date and in full data set
+    recent_seg_count = pd.Series(recent_efforts_df.groupby('athlete_id').count().segment_id)
+    tot_seg_count = pd.Series(df.groupby('athlete_id').count().segment_id.ix[recent_seg_count.index])
 
-    testing_df = df.ix[drop_mask]
-    training_df = df.drop(drop_mask)
+    # Grab only athletes who ridden on other segments before "date"
+    eligible_athletes = recent_seg_count[~(recent_seg_count == tot_seg_count)].index
+    test_mask = [index for athlete in eligible_athletes
+                       for index in recent_efforts_df.query('athlete_id == {}'.format(athlete)
+                                                                             ).index.values]
+
+    # Make training and testing subset dfs of the full df from eligble athlete mask
+    testing_df = df.ix[test_mask]
+    training_df = df.drop(test_mask)
 
     return training_df, testing_df
 
@@ -58,9 +72,16 @@ def testing_rmse(model, testing_df):
     '''
     Input: Trained GraphLab recommender model, Test observation DataFrame
     Output: RMSE for testing_df
+
+    Get the root mean squared error for the test data's predicted values from the model
     '''
+    # Make appropriate SFrame out of testing data for prediction
     test_sf = gl.SFrame(testing_df[['athlete_id', 'segment_id', 'average_speed']])
+
+    # Predict on testing data SFrame with model
     predictions = np.array(model.predict(test_sf))
+
+    # Calculate root mean squared error between actual test data and predicted values from model
     rmse = (((testing_df.average_speed.values - predictions) ** 2) ** 0.5).sum() / \
                                                                     predictions.shape[0]
     return rmse
