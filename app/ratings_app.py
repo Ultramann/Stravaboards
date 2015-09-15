b70da8d0891dcad468a5387b9ef1c9e5de2fe0e6
Ultramann <cary.goltermann@gmail.com>
2015-09-15 23:48:33 +0000

    Working with new csv files in app

--- app/ratings_app.py
+++ app/ratings_app.py
@@ -1,25 +1,26 @@
-import pickle
+import os
+import pandas as pd
 from flask import Flask, render_template #, request
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/leaderboards')
 def display_leaderboards():
-    # Unpickle leaderboard list
-    lb_file_path = 'app_data/leaderboards.pkl'
-    with open(lb_file_path, 'r') as f:
-        df_leaderboards = pickle.load(f)
+    # Make leaderboard list from csvs
+    app_data = '../app_data/'
+    csv_list = os.listdir(app_data)
+    leaderboards = [pd.read_csv(app_data + file_name) for file_name in csv_list]
 
     # Turn df list into nparray list
-    npa_leaderboards = [df_leaderboard.reset_index().values for df_leaderboard in df_leaderboards]
+    #npa_leaderboards = [df_leaderboard.reset_index().values for df_leaderboard in df_leaderboards]
 
     # Make list of leaderboard lists with correct data types
-    leaderboards = [[[int(row[0]), int(row[1]), round(float(row[2]), 3)] for row in leaderboard] 
-                                                                for leaderboard in npa_leaderboards]
+    #leaderboards = [[[int(row[0]), int(row[1]), round(float(row[2]), 3)] for row in leaderboard] 
+    #                                                            for leaderboard in npa_leaderboards]
 
     return render_template('leaderboards.html', 
                             leaderboards_and_names=zip(leaderboards, 
-                                                       ['Top', 'Second', 'Third', 'Last']))
+                                                       [name[:-13] for name in csv_list]))
 
 if __name__ == '__main__':
     app.run(debug=True)
